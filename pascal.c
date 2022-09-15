b364b57835dab186200e9e3d70dae6521136f42b
Stephen Rice <sarice8@gmail.com>
2022-09-14 17:15:30 -0700

    Cleanup: remove pre-migration code

--- pascal.c
+++ pascal.c
@@ -33,14 +33,15 @@
 #include "pascal.tbl"
 
 
-/* The scanner is hardcoded in C.
+/* The scanner is part of ssl_rt 2.0
+   with configuration done here to (ideally) match pascal requirements.
+   It's still not 100% correct though.
 
    Scanner variables start with 't_'.
    Table walker variables start with 'w_'.
    Semantic operations start with 'o'.
    Corresponding data structures start with 'd'.
 
-   The code to walk is in w_codeTable (pointer=w_pc).
    The output buffer is w_outTable (pointer=w_here).
 
    A token is read and screened when oInput, oInputAny, or oInputChoice
@@ -64,72 +65,8 @@
 FILE *t_out,*t_doc,*t_lst;
 
 #define t_lineSize 256
-#define t_strSize 256
-#define t_nameSize 31
-#define t_idTableSize 400
-
 char   t_lineBuffer[t_lineSize];
-char   t_lineBuffer2[t_lineSize];
-char   *t_ptr;
-short  t_lineNumber;
-char   t_lineListed;
 char   t_listing;                  /* do we want a listing file? */
-
-/* token codes and id kinds are declared in the SSL program, not here */
-
-char t_upper[256],t_blank[256],t_idStart[256],t_idCont[256];
-char t_digit[256];
-short t_punct[256];
-
-#define t_idBufferSize 5000
-char t_idBuffer[t_idBufferSize];  /* id names stored here.. avg 12 chars */
-short t_idBufferNext;
-
-struct t_idTableType {         /* identifier # is index into this table */
-  char *name;
-  short namelen;
-  short code;                  /* keyword code, or 'pIdent' */
-} t_idTable[t_idTableSize];
-short t_idTableNext;
-
-struct t_kwTableType {
-  char *name;
-  short code;
-} t_kwTable[] = {
-  "program",pProgram,
-  "procedure",pProcedure,
-  "function",pFunction,
-  "const",pConst,
-  "type",pType,
-  "var",pVar,
-  "begin",pBegin,
-  "end",pEnd,
-  "array",pArray,
-  "record",pRecord,
-  "set",pSet,
-  "of",pOf,
-  "if",pIf,
-  "then",pThen,
-  "else",pElse,
-  "for",pFor,
-  "to",pTo,
-  "downto",pDownto,
-  "do",pDo,
-  "while",pWhile,
-  "repeat",pRepeat,
-  "until",pUntil,
-  "cycle",pCycle,
-  "exit",pExit,
-  "return",pReturn,
-  "and",pAnd,
-  "or",pOr,
-  "not",pNot,
-  "writeln",pWriteln,
-  "write",pWrite,
-  "readln",pReadln,
-  "read",pRead,
-  "",0
-};
 
 
 // Forward declarations
@@ -138,29 +75,13 @@ void close_my_files();
 void init_my_scanner();
 void init_my_operations();
 
-void t_initScanner ();
-void t_getNextToken();
-short lookup(short id);
 void t_printToken();
 void t_dumpTables();
 
-void w_walkTable();
-
-
-
-
-/* Variables for table walker */
-
-#define w_stackSize 200
-short w_stack[w_stackSize],w_sp;   /* Call stack */
-short w_pc;                        /* Program counter */
-short w_options;                   /* # choice options */
-char w_errBuffer[256];             /* build up err messages here */
 
 #define w_outTableSize 5000
 short w_outTable[w_outTableSize];  /* Emit into this table */
 short w_here;                      /* Output position */
-short w_errorCount;                /* Number of error signals emitted */
 
 /*************** Variables for semantic mechanisms ***************/
 
@@ -168,31 +89,6 @@ short dTemp;                       /* multi-purpose */
 short *dPtr;                       /* multi-purpose */
 short dWords;                      /* multi-purpose */
 
-short dSym;                        /* index of looked-up symbol in ST*/
-
-struct dStype {                    /* entry for symbol table, symbol stack */
-  short id;        /* id# of name */
-  short kind;      /* kVar, kProgram, etc */
-  short val;       /* constant value */
-  short paramTyp;  /* type of proc/func params */
-                   /*   (tyNone, or type# of a type of class 'tyParams') */
-  short typ;       /* type of var, type, func */
-};
-
-#define dSTsize 200
-struct dStype dST[dSTsize];               /* symbol table */
-short dSTptr;
-
-#define dSDsize 15
-struct dSDtype {            /* symbol table display */
-  short sym, allocOffset;
-} dSD[dSDsize];
-short dSDptr;
-
-// symbol stack
-#define dSSsize 40
-struct dStype dSS[dSSsize];
-short dSSptr;
 
 // scope stack
 #define dScopeStackSize 40
@@ -200,24 +96,8 @@ NODE_PTR        dScopeStack[dScopeStackSize];
 int             dScopeStackPtr;
 
 
-// OLD:
-#define dTTsize 50
-struct dTTtype {          /* type table */
-  short kind;             /* for user types: array, record, set, etc */
-  short size;             /* #words required for this type */
-  short low, high;        /* range of an array dimension */
-  short base;             /* base type of this type (e.g. array OF-type) */
-  short ptrType;          /* type# of pointer to this type (0 if none yet) */
-} dTT[dTTsize];
-short dTTptr;
-short dLastScalar;        /* typ# of last basic type (user typ#'s follow) */
-
-// OLD:
-#define dTSsize 40
-short dTS[dTSsize], dTSptr;        /* type stack ... index into TT */
-
-// New type table, owns nType nodes.
-// Need to make dynamic size.
+// Type table, owns nType nodes.
+// TO DO: make all of these tables dynamic size.
 #define dTypeTableSize 50
 NODE_PTR dTypeTable[dTypeTableSize];
 int dTypeTablePtr;
@@ -315,15 +195,6 @@ char *argv[];
   if (status != 0) {
     exit( -1 );
   }
-
-#if 0
-  if (w_errorCount)
-    printf("\n%d error(s)\n",w_errorCount);
-
-  t_cleanup();
-  if (w_errorCount)
-    exit(5);
-#endif
 
   exit( 0 );
 }
@@ -434,62 +305,8 @@ init_my_operations()
   // Initialize node package (schema runtime module)
   nodeInit();
 
-  /* Symbol display level 0 is for global symbols.
-     Symbol table entry 0 is unused; it is referenced when an undefined
-     name is searched for. */
-
-  dSD[0].sym = 1;
-  dSD[0].allocOffset = 0;     /* offset of next var to allocate */
-  dST[0].kind = kUndefined;
-  dSTptr = 0;
-
-  /* built-in types */
-  /* note, entry 0 of TT is not used */
-
-#if 0
-  dST[++dSTptr].id = ssl_add_id("integer",pIdent);
-  dST[dSTptr].kind = kType;
-  dST[dSTptr].typ  = tyInteger;
-  dTT[tyInteger].size = 1;
-
-  dST[++dSTptr].id = ssl_add_id("boolean",pIdent);
-  dST[dSTptr].kind = kType;
-  dST[dSTptr].typ  = tyBoolean;
-  dTT[tyBoolean].size = 1;
-
-  dST[++dSTptr].id = ssl_add_id("char",pIdent);
-  dST[dSTptr].kind = kType;
-  dST[dSTptr].typ  = tyChar;
-  dTT[tyChar].size = 1;
-
-  dST[++dSTptr].id = ssl_add_id("string",pIdent);
-  dST[dSTptr].kind = kType;
-  dST[dSTptr].typ  = tyString;
-  dTT[tyString].size = 128;  /* words */
-
-  dST[++dSTptr].id = ssl_add_id("file",pIdent);
-  dST[dSTptr].kind = kType;
-  dST[dSTptr].typ  = tyFile;
-  dTT[tyFile].size = 1;
-#endif
-
-  dTTptr = tyFile;
-  dLastScalar = tyFile;
-
-  /* built-in constants */
-
-#if 0
-  dST[++dSTptr].id = ssl_add_id("true",pIdent);
-  dST[dSTptr].kind = kConst;
-  dST[dSTptr].typ  = tyBoolean;
-  dST[dSTptr].val  = 1;
-
-  dST[++dSTptr].id = ssl_add_id("false",pIdent);
-  dST[dSTptr].kind = kConst;
-  dST[dSTptr].typ  = tyBoolean;
-  dST[dSTptr].val  = 0;
-#endif
-
+  // Note: global scope will be created by the ssl program,
+  // as will predefined types and constants.
 }
 
 
@@ -511,8 +328,6 @@ NODE_PTR dNode;  // temporary for several mechanisms
             dTemp = ssl_code_table[ssl_pc++];
             switch (dTemp) {
               case tSpace :      w_outTable[w_here++] = 0;
-                                 continue;
-              case tSymVal :     w_outTable[w_here++] = dST[dSym].val;
                                  continue;
               default :          w_outTable[w_here++] = dTemp;
                                  continue;
@@ -794,42 +609,12 @@ NODE_PTR dNode;  // temporary for several mechanisms
 
      /* Mechanism value */
 
-     case oValuePushToken :
-            if (++dVSptr==dVSsize) ssl_fatal("VS overflow");
-            dVS[dVSptr] = ssl_token.val;
-            continue;
-     case oValuePushVal :
-            if (++dVSptr==dVSsize) ssl_fatal("VS overflow");
-            dVS[dVSptr] = dST[dSym].val;
-            continue;
      case oValuePush :
             if (++dVSptr==dVSsize) ssl_fatal("VS overflow");
             dVS[dVSptr] = ssl_param;
             continue;
-     case oValuePushSizeTS :
-            if (++dVSptr==dVSsize) ssl_fatal("VS overflow");
-            dVS[dVSptr] = dTT[dTS[dTSptr]].size;
-            continue;
-     case oValuePushLowTS :
-            if (++dVSptr==dVSsize) ssl_fatal("VS overflow");
-            dVS[dVSptr] = dTT[dTS[dTSptr]].low;
-            continue;
      case oValueNegate :
             dVS[dVSptr] *= -1;
-            continue;
-     case oValueDifference :
-            dVS[dVSptr-1] = dVS[dVSptr] - dVS[dVSptr-1] + 1;
-            dVSptr--;
-            continue;
-     case oValueMultiply :
-            dVS[dVSptr-1] *= dVS[dVSptr];
-            dVSptr--;
-            continue;
-     case oValueIsZero :
-            ssl_result = !dVS[dVSptr];
-            continue;
-     case oValueIsOne :
-            ssl_result = (dVS[dVSptr] == 1);
             continue;
      case oValueTop:
             ssl_result = dVS[dVSptr];
@@ -874,63 +659,6 @@ NODE_PTR dNode;  // temporary for several mechanisms
             continue;
             }
 
-     /* Mechanism typ */
-
-     case oTypSPushTyp :
-            if (++dTSptr==dTSsize) ssl_fatal("TS overflow");
-            dTS[dTSptr] = dST[dSym].typ;
-            continue;
-     case oTypSPush :
-            if (++dTSptr==dTSsize) ssl_fatal("TS overflow");
-            dTS[dTSptr] = ssl_param;
-            continue;
-     case oTypSPopPushBase :
-            dTS[dTSptr] = dTT[dTS[dTSptr]].base;
-            continue;
-     case oTypSPopPushPtr :
-            dTS[dTSptr] = dTT[dTS[dTSptr]].ptrType;
-            continue;
-     case oTypSMatch :     /* actually, should follow to roots of types */
-            ssl_result = (dTS[dTSptr] == dTS[dTSptr-1]);
-            dTSptr--;
-            continue;
-     case oTypSChoose :
-            ssl_result = dTS[dTSptr];
-            continue;
-     case oTypSChoosePop :
-            ssl_result = dTS[dTSptr--];
-            continue;
-     case oTypSChooseKind :
-            ssl_result = dTT[dTS[dTSptr]].kind;
-            continue;
-     case oTypSChoosePtr :
-            ssl_result = dTT[dTS[dTSptr]].ptrType;
-            continue;
-     case oTypSSwap :
-            dTemp = dTS[dTSptr-1];
-            dTS[dTSptr-1] = dTS[dTSptr];
-            dTS[dTSptr] = dTemp;
-            continue;
-     case oTypSPop :
-            dTSptr--;
-            continue;
-     case oTypNew :
-            if (++dTTptr==dTTsize) ssl_fatal("TT overflow");
-            if (++dTSptr==dTSsize) ssl_fatal("TS overflow");
-            dTT[dTTptr].kind = ssl_param;
-            dTT[dTTptr].ptrType = tyNone; /* no ptr to this type defined */
-            dTS[dTSptr] = dTTptr;
-            continue;
-     case oTypSetSize :
-            dTT[dTS[dTSptr]].size = dVS[dVSptr];
-            continue;
-     case oTypAssignBasePop :
-            dTT[dTS[dTSptr-1]].base = dTS[dTSptr];
-            dTSptr--;
-            continue;
-     case oTypAssignPtr :
-            dTT[dTS[dTSptr-1]].ptrType = dTS[dTSptr];
-            continue;
 
      /* Mechanism patch */
 
@@ -961,284 +689,11 @@ NODE_PTR dNode;  // temporary for several mechanisms
             w_here++;
             continue;
 
-     /* Mechanism sym */
-
-     case oSymPushLevel :
-            if (++dSDptr==dSDsize) ssl_fatal("SD overflow");
-            dSD[dSDptr].sym = dSTptr + 1;
-            dSD[dSDptr].allocOffset = 0;
-            continue;
-     case oSymPopLevel :
-            dSTptr = dSD[dSDptr--].sym - 1;
-            continue;
-     case oSymPopLevelSaveType :
-            /* attach id's at top level to syms field of top type stack */
-            /****/
-            dSTptr = dSD[dSDptr--].sym - 1;
-            continue;
-     case oSymLookup :
-            dSym = lookup(ssl_last_id);
-            continue;
-     case oSymChooseKind :
-            ssl_result = dST[dSym].kind;
-            continue;
-     case oSymAddSPop :
-            if (++dSTptr==dSTsize) ssl_fatal("ST overflow");
-            dST[dSTptr] = dSS[dSSptr--];
-            continue;
-     case oSymLevelAnySyms :
-            ssl_result = dSTptr >= dSD[dSDptr].sym;
-            continue;
-     case oSymSPushId :
-            if (++dSSptr==dSSsize) ssl_fatal("SS overflow");
-            dSS[dSSptr].id = ssl_last_id;
-            /* perhaps clear other fields, for debugging */
-            continue;
-     case oSymSSetKind :
-            dSS[dSSptr].kind = ssl_param;
-            continue;
-     case oSymSSetValPop :
-            dSS[dSSptr].val = dVS[dVSptr--];
-            continue;
-     case oSymSSetTypS :
-            dSS[dSSptr].typ = dTS[dTSptr];
-            continue;
-     case oSymSSetParamTypS :
-            dSS[dSSptr].paramTyp = dTS[dTSptr];
-            continue;
-     case oSymSAllocate :
-            dSS[dSSptr].val = dSD[dSDptr].allocOffset;
-            dSD[dSDptr].allocOffset += dTT[dTS[dTSptr]].size;
-            continue;
-
 
 #include "ssl_end.h"
 
 // ---------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------
-
-
-
-/* Search the symbol table for the id given.  If found, return index in
-   table.  Otherwise, return 0. */
-
-short
-lookup( short id )
-{
-  short i;
-  for (i=dSTptr; i>=1; i--)
-    if (dST[i].id == id)
-      return(i);
-  return(0);
-}
-
-
-#if 0
-// ---------------------------------------------------------------------------------------
-// Pre-2.0
-// ---------------------------------------------------------------------------------------
-
-/************************ S c a n n e r *********************/
-
-void
-t_initScanner ()
-{
-  register short i;
-  short addId();
-
-  /* C guarantees static vars to start with 0, so here I don't
-       bother to initialize all of blank,idStart,idCont,digit */
-
-  for (i=0;i<256;i++)
-    t_upper[i] = i;
-  for (i='a';i<='z';i++) {
-    t_upper[i] = i - ('a'-'A');
-    t_idStart[i] = 1;
-    t_idCont[i] = 1;
-  }
-  for (i='A';i<='Z';i++) {
-    t_idStart[i] = 1;
-    t_idCont[i] = 1;
-  }
-  t_idStart['_'] = 1;
-  t_idCont['_'] = 1;
-  t_idStart['$'] = 1;
-  t_idCont['$'] = 1;
-  for (i='0';i<='9';i++) {
-    t_digit[i] = 1;
-    t_idCont[i] = 1;
-  }
-  t_blank[' '] = 1;
-  t_blank['\t'] = 1;
-  t_blank['\n'] = 1;
-
-  /* single-character tokens */
-  for (i=0;i<256;i++)
-    t_punct[i] = pInvalid;
-  t_punct['='] = pEqual;
-  t_punct[';'] = pSemiColon;
-  t_punct[':'] = pColon;
-  t_punct[','] = pComma;
-  t_punct['('] = pLParen;
-  t_punct[')'] = pRParen;
-  t_punct['['] = pLSquare;
-  t_punct[']'] = pRSquare;
-  t_punct['^'] = pCarat;
-  t_punct['*'] = pTimes;
-  t_punct['/'] = pDivide;
-  t_punct['+'] = pPlus;
-  t_punct['-'] = pMinus;
-  t_lineBuffer[0] = '\0';
-  t_ptr = t_lineBuffer;
-  t_lineNumber = 0;
-  t_lineListed = 1;
-  t_idTableNext = 0;
-  ssl_token.code = pColon;           /* dummy */
-  ssl_token.accepted = 1;
-  for (i=0; *t_kwTable[i].name; i++)
-    (void) t_addId(t_kwTable[i].name,t_kwTable[i].code);
-}  
-
-
-void
-t_getNextToken()
-{
-  char *p;
-  short lit;
-
-  ssl_token.accepted = 0;
-
-  /* error if we try to read past the end of file */
-  if (ssl_token.code == pEof)              /* last token was eof */
-    ssl_fatal ("Unexpected end of file"); 
-
-  /* skip to start of token */
-
-  while (1) {
-    while (t_blank[*t_ptr])
-      t_ptr++;
-    if (!*t_ptr) {
-      if (t_listing && !t_lineListed)
-        fprintf(t_lst,"      %s",t_lineBuffer);
-      if (!fgets(t_lineBuffer,t_lineSize,t_src))
-            {  ssl_token.code = pEof; return;  }
-      t_ptr = t_lineBuffer;
-      t_lineNumber++;
-      t_lineListed = 0;
-    } else if (*t_ptr=='{') {
-      t_ptr++;
-      while (*t_ptr!='}') {
-        if (!*t_ptr++) {
-          if (t_listing && !t_lineListed)
-            fprintf(t_lst,"      %s",t_lineBuffer);
-          if (!fgets(t_lineBuffer,t_lineSize,t_src))
-                {  ssl_token.code = pEof; return;  }
-          t_ptr = t_lineBuffer;
-          t_lineNumber++;
-          t_lineListed = 0;
-        }
-      }
-      t_ptr++;
-      continue;
-    } else {
-      break;
-    }
-  }
-
-  /* copy token, screen ids and integers */
-
-  ssl_token.lineNumber = t_lineNumber;
-  ssl_token.colNumber = t_ptr-t_lineBuffer;    /* from 0, for err msgs */
-  p = ssl_token.name;
-  if (t_idStart[*t_ptr]) {
-    while (t_idCont[*t_ptr] && p-ssl_token.name < t_nameSize)
-      *p++ = *t_ptr++;
-    *p = '\0';
-    ssl_token.namelen = p-ssl_token.name;
-    ssl_token.code = pIdent;
-    t_lookupId();
-  } else if (t_digit[*t_ptr]) {
-    lit = 0;
-    while (t_digit[*t_ptr]) {
-      lit = lit*10 - '0' + *t_ptr;
-      *p++ = *t_ptr++;
-    }
-    *p = '\0';
-    ssl_token.val = lit;
-    ssl_token.code = pIntLit;
-  } else if (*t_ptr=='\'') {
-    p = ssl_strlit_buffer;
-    t_ptr++;
-    ssl_token.code = pStrLit;
-    while (p-ssl_strlit_buffer < t_strSize) {
-      if (*t_ptr=='\'') {
-        if (*++t_ptr=='\'') {
-          *p++ = '\'';
-          t_ptr++;
-        } else {
-          break;
-        }
-      } else if (*t_ptr=='\0') {
-        break;
-      } else {
-        *p++ = *t_ptr++;
-      }
-    }
-    *p = '\0';
-    ssl_token.namelen = p-ssl_strlit_buffer;
-  } else if (*t_ptr==':') {
-    if (*++t_ptr=='=') {
-      t_ptr++;
-      ssl_token.code = pAssign;
-    } else {
-      ssl_token.code = pColon;
-    }
-  } else if (*t_ptr=='<') {
-    if (*++t_ptr=='>') {
-      t_ptr++;
-      ssl_token.code = pNotEqual;
-    } else if (*t_ptr=='=') {
-      t_ptr++;
-      ssl_token.code = pLessEqual;
-    } else {
-      ssl_token.code = pLess;
-    }
-  } else if (*t_ptr=='>') {
-    if (*++t_ptr=='=') {
-      t_ptr++;
-      ssl_token.code = pGreaterEqual;
-    } else {
-      ssl_token.code = pGreater;
-    }
-  } else if (*t_ptr=='.') {
-    if (*++t_ptr=='.') {
-      t_ptr++;
-      ssl_token.code = pDotDot;
-    } else {
-      ssl_token.code = pDot;
-    }
-  } else {
-    ssl_token.code = t_punct[*t_ptr++];
-    if (ssl_token.code == pInvalid) {
-      --t_ptr;
-      ssl_token.name[0] = *t_ptr++;
-      ssl_token.name[1] = '\0';
-      ssl_error ("Invalid character");
-    }
-  }
-}
-
-
-int
-t_hitBreak()
-{
-  printf("Breaking...\n");
-  t_cleanup();
-  return(1);
-}
-
-#endif
-
 
 
 void
